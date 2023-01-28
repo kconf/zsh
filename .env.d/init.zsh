4ce0a9a937b0654a3521bccfd61e60d177d94b77
Jianwei Han <hanjianwei@gmail.com>
2023-01-28 09:19:48 +0800

    Initial commit

---
+++ .env.d/init.zsh
@@ -0,0 +1,182 @@
+#!/bin/zsh -f
+#
+# Useful aliases and tools
+#
+
+# List files
+if (( $+commands[lsd] )); then
+  alias ls=lsd
+elif (( $+commands[exa] )); then
+  alias ls=exa
+else
+  case "$OSTYPE" in
+    darwin*|freebsd* )
+      alias ls='ls -G'
+      ;;
+    linux* )
+      alias ls='ls --color=auto'
+      ;;
+  esac
+fi
+
+alias l=ls
+alias ll='ls -lh'
+alias la='ls -a'
+
+# Fuzzy finder
+if (( $+commands[fzf] )); then
+  case "$OSTYPE" in
+    darwin*)
+      source "/usr/local/opt/fzf/shell/completion.zsh"
+      source "/usr/local/opt/fzf/shell/key-bindings.zsh"
+      ;;
+    linux*)
+      source "/usr/share/fzf/completion.zsh"
+      source "/usr/share/fzf/key-bindings.zsh"
+      ;;
+  esac
+
+  export FZF_DEFAULT_COMMAND='fd --type f --hidden --follow --exclude .git'
+  export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
+  alias f='fzf-tmux -r 60%'
+  alias fp='f --height=70% --preview="head -80 {}" --preview-window=right:60%:wrap'
+fi
+
+zz() {
+  cd $(z -l $@ | awk '{print $2}' | f)
+}
+
+# Git
+alias g=git
+
+# Editors
+e() {
+  zparseopts -D t=o_tty
+
+  if [[ $o_tty == '-t' || ! -v DISPLAY ]]; then
+    emacsclient -t "$@"
+  else
+    if (( $(emacsclient -n -e "(length (frame-list))") > 1 )); then
+      emacsclient -nq "$@" &> /dev/null
+    else
+      emacsclient -nqc "$@" &> /dev/null
+    fi
+  fi
+}
+
+alias et='e -t'
+alias v=vim
+alias n=nvim
+
+# Proxy using clash
+p() {
+  local http_port=8889
+  local socks_port=1089
+  if (( $# == 0 )); then
+    unset http_proxy https_proxy all_proxy no_proxy GIT_SSH_COMMAND
+  else
+    export http_proxy=http://$1:$http_port https_proxy=http://$1:$http_port all_proxy=socks5://$1:$socks_port
+    export no_proxy="localhost,*.local,127.0.0.1,10.0.0.0/8,172.16.0.0/12,192.168.0.0/16,::1,fe80::/10"
+    export GIT_SSH_COMMAND="ssh -o 'ProxyCommand=nc -x $1:$socks_port %h %p'"
+  fi
+  env | grep proxy
+}
+
+_p() {
+  _arguments '1: :(localhost n1)'
+}
+compdef _p p
+
+# Config management
+c() {
+  if (( $# < 2 )); then
+    echo "Usage: $0 <diff|backup|update> pkg"
+    return
+  fi
+
+  local act=$1
+  local pkg=$2
+  local cfg=$HOME/setup/roles/$pkg/files
+
+  if [[ ! -d $cfg ]]; then
+    echo "No files for $pkg"
+    return
+  fi
+
+  pushd -q $cfg
+  local files=($(rg --hidden --files))
+
+  if [[ $act == diff ]]; then
+    for f in ${files[@]}; do
+      if [[ -e $HOME/$f ]]; then
+        git diff --no-index -w $cfg/$f $HOME/$f
+      else
+        echo "$HOME/$f \e[31mX\e[m"
+      fi
+    done
+  elif [[ $act == backup ]]; then
+    for f in ${files[@]}; do
+      if [[ -e $HOME/$f ]]; then
+        cp $HOME/$f $cfg/$f
+      fi
+    done
+  elif [[ $act == update ]]; then
+    for f in ${files[@]}; do
+      cp $cfg/$f $HOME/$f
+    done
+  else
+    echo "Usage: $0 <diff|backup|update> [pkg]"
+  fi
+
+  popd -q
+}
+
+_c() {
+  local state
+  _arguments '1: :(diff backup update)' '2: :->pkg'
+
+  if [[ $state == pkg ]]; then
+    _describe 'command' "($(ls $HOME/setup/roles))"
+  fi
+}
+
+compdef _c c
+
+# http server
+alias s='python3 -m http.server & open http://localhost:8000 && fg'
+
+# Disk
+d() {
+  local f=${1:-$PWD}
+  local depth=${2:-0}
+  du -d $depth -h $f | sort -rh
+}
+
+# Cheat sheet
+alias cs='open $(find $HOME/cheatsheets -type f|fzf)'
+
+# Open
+case "$OSTYPE" in
+  darwin*)
+    alias o=open
+    ;;
+  linux*)
+    alias o=xdg-open
+    ;;
+esac
+
+for f in $HOME/.env.d/*.plugin.zsh(-.N); do
+  source $f
+done
+unset f
+
+# Kitty
+alias kk='kitty +kitten'
+# Ranger
+alias rr=ranger
+# Label me
+lm() {
+    labelme $1 -O ${1:r}.json
+}
+# Patch Android Studio to avoid case-sensitive warning
+alias intellij-case-patch='printf '\''\nidea.case.sensitive.fs=true'\'' >> /Applications/Android\ Studio.app/Contents/bin/idea.properties'
---
+++ .zprofile
@@ -0,0 +1,49 @@
+#
+# Defines environment variables.
+#
+
+# Browser (Default)
+if [[ "$OSTYPE" == darwin* ]]; then
+  export BROWSER='open'
+fi
+
+# Editors
+export EDITOR='nvim'
+export VISUAL='nvim'
+export PAGER='less'
+
+# Language
+if [[ -z "$LANG" ]]; then
+  export LANG='en_US.UTF-8'
+fi
+
+# Less
+# Set the default Less options.
+export LESS='-F -g -i -M -R -S -w -X -z-4'
+
+# Paths
+typeset -gU cdpath fpath mailpath path
+
+# Set the list of directories that Zsh searches for programs.
+path=(
+  /usr/local/{bin,sbin}
+  /usr/{bin,sbin}
+  /{bin,sbin}
+  $path
+)
+
+if [[ -d $HOME/bin ]]; then
+  path+=($HOME/bin)
+fi
+
+# User defined env
+for env_file in $HOME/.env.d/*.env(-.N); do
+  source $env_file
+done
+unset env_file
+
+# Skip the not really helping Ubuntu global compinit
+skip_global_compinit=1
+
+# Created by `pipx` on 2022-12-08 02:41:15
+export PATH="$PATH:/Users/hjw/.local/bin"
