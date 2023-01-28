54ccc4dfefc3c65ffc0defe1cd9242544959595d
Jianwei Han <hanjianwei@gmail.com>
2023-01-28 09:19:48 +0800

    Initial commit

---
+++ install.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+sudo pacman -S --needed --noconfirm zsh fzf zoxide starship eza bat ripgrep zellij fd sheldon trash-cli
+sudo pacman -S --needed --noconfirm nnn unarchiver atool zip unzip nsxiv zathura
