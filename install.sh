a109e1debb165d9544a7c101a1a6b120a3cc4b34
Jianwei Han <hanjianwei@gmail.com>
2024-07-16 22:17:56 +0800

    Add nnn plugins

--- install.sh
+++ install.sh
@@ -2,3 +2,4 @@
 
 sudo pacman -S --needed --noconfirm zsh fzf zoxide starship eza bat ripgrep zellij fd sheldon trash-cli
 sudo pacman -S --needed --noconfirm nnn unarchiver atool zip unzip nsxiv zathura
+sh -c "$(curl -Ls https://raw.githubusercontent.com/jarun/nnn/master/plugins/getplugs)"
