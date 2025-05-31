e2f525a41e0f9eaa622065154c7aa00eaf939ce8
Jianwei Han <hanjianwei@gmail.com>
2025-06-01 00:52:49 +0800

    build(install): update package list in install script

--- install.sh
+++ install.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
 
-sudo pacman -S --needed --noconfirm zsh fzf zoxide starship eza bat ripgrep zellij fd sheldon trash-cli
-sudo pacman -S --needed --noconfirm nnn unarchiver atool zip unzip nsxiv zathura
-sh -c "$(curl -Ls https://raw.githubusercontent.com/jarun/nnn/master/plugins/getplugs)"
+sudo pacman -S --needed --noconfirm zsh fzf zoxide starship eza bat ripgrep zellij fd sheldon atuin
+sudo pacman -S --needed --noconfirm yazi ouch unarchiver atool zip unzip nsxiv zathura zathura-pdf-mupdf
