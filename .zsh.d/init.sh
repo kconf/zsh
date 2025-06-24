ffd58f0b7b68f0783ceb1d2bfe5d9385f5222790
Jianwei Han <hanjianwei@gmail.com>
2025-06-24 20:16:53 +0800

    refactor(zsh): improve proxy management function

--- .zsh.d/init.sh
+++ .zsh.d/init.sh
@@ -6,23 +6,40 @@ qr() { curl qrenco.de/$* }
 
 clone() { gh repo clone $1 $HOME/Dev/github.com/$1 }
 
-
-proxy_on() {
-	export no_proxy="localhost,127.0.0.1,::1,192.168.0.0/16,localaddress,.localdomain.com"
-
-	local proxy=$1
-	export http_proxy="$proxy" \
-		https_proxy=$proxy \
-		ftp_proxy=$proxy \
-		rsync_proxy=$proxy \
-		HTTP_PROXY=$proxy \
-		HTTPS_PROXY=$proxy \
-		FTP_PROXY=$proxy \
-		RSYNC_PROXY=$proxy
-}
-
-proxy_off() {
-	unset http_proxy https_proxy ftp_proxy rsync_proxy \
-		HTTP_PROXY HTTPS_PROXY FTP_PROXY RSYNC_PROXY
-	echo -e "Proxy environment variable removed."
+proxy() {
+	local default_proxy="http://localhost:7890"
+	
+	case "$1" in
+		"on")
+			local proxy_url="${2:-$default_proxy}"
+			export no_proxy="localhost,127.0.0.1,::1,192.168.0.0/16,localaddress,.localdomain.com"
+			export http_proxy="$proxy_url" \
+				https_proxy="$proxy_url" \
+				ftp_proxy="$proxy_url" \
+				rsync_proxy="$proxy_url" \
+				HTTP_PROXY="$proxy_url" \
+				HTTPS_PROXY="$proxy_url" \
+				FTP_PROXY="$proxy_url" \
+				RSYNC_PROXY="$proxy_url"
+			echo "Proxy enabled: $proxy_url"
+			;;
+		"off")
+			unset http_proxy https_proxy ftp_proxy rsync_proxy \
+				HTTP_PROXY HTTPS_PROXY FTP_PROXY RSYNC_PROXY no_proxy
+			echo "Proxy disabled."
+			;;
+		"toggle")
+			if [[ -n "$http_proxy" ]]; then
+				proxy off
+			else
+				proxy on
+			fi
+			;;
+		*)
+			echo "Usage: proxy {on [url]|off|toggle}"
+			echo "  proxy on [url]  - Enable proxy (default: $default_proxy)"
+			echo "  proxy off       - Disable proxy"
+			echo "  proxy toggle    - Toggle proxy state"
+			;;
+	esac
 }
